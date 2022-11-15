5611106cab7e905350cee6d3637a99e381007ce5
Sophie Sun <sophiesun2810@gmail.com>
2022-11-15 10:33:11 -0500

    class and events: update them together

--- fitness_club/studios/models.py
+++ fitness_club/studios/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from location_field.models.plain import PlainLocationField
-
+from datetime import datetime, timedelta
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Studio(models.Model):
     name = models.CharField(max_length=50,
@@ -37,16 +40,48 @@ class Amenity(models.Model):
     quantity = models.PositiveIntegerField(blank=False, null=False)
 
 class Class(models.Model):
-    studio = models.ForeignKey(Studio, on_delete=models.CASCADE)
+    studio = models.ForeignKey(Studio, on_delete=models.CASCADE, related_name='classes')
     name = models.CharField(max_length=50, blank=False, null=False)
     description = models.TextField(blank=True, null=True)
     capacity = models.IntegerField(blank=False, null=False)
 
-    start_time = models.DateTimeField(blank=False, null=False)
-    end_time = models.DateTimeField(blank=False, null=False)
+    start_date = models.DateTimeField(blank=False, null=False)#blank=False, null=False
+    end_date = models.DateTimeField(blank=False, null=False) #blank=False, null=False
+    duration = models.PositiveIntegerField(blank=False, null=False)
+
+    curr_capacity = models.PositiveBigIntegerField(default=capacity)
 
     def __str__(self):
         return str(self.studio) + " " + self.name
+
+
+@receiver(post_save, sender=Class)
+def createEvents(sender, **kwargs):
+    # create all valid events based on sender's start and end dates 
+    # handle differently if time is changed, should destroy all previous associated events and create a series of new ones
+
+        # def save(self, *args, **kwargs):
+    #     pk = self.pk
+    #     # create associated events upon creating new class
+    #     if not self.pk: 
+    #         curr = self.start_date
+    #         while curr < timezone.now():
+    #             curr = curr + timedelta(days = 7)
+    #         while curr < self.end_date:
+    #             e=Event(belonged_class=self.id, time=curr)
+    #             e.save()
+    #             curr = curr + timedelta(days = 7)
+
+    #     super(Class, self).save(*args, **kwargs)
+    return
+
+
+
+
+
+class Event(models.Model):
+    belonged_class = models.ForeignKey(Class, on_delete=models.CASCADE, related_name='events')
+    time = models.DateTimeField(blank=False, null=False)
 
 
 class Tag(models.Model):
--- fitness_club/studios/serializers/studio_detail.py
+++ fitness_club/studios/serializers/studio_detail.py
@@ -1,16 +1,32 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
-from location_field.models.plain import PlainLocationField
-from studios.models import Studio, StudioImage, Amenity
-
+from studios.models import Studio, StudioImage, Amenity, Class, Event
+from datetime import datetime, timedelta
 from django.utils.http import urlencode
+
+from django.utils import timezone
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ('time')
+
+class ClassSerializer(serializers.ModelSerializer):
+    events = EventSerializer(many=True, read_only=True)
+    class Meta:
+        model = Class
+        fields = ('name', 'description','curr_capacity', 'capacity', 'duration', 'events')
+
 
 class StudioSerializer(serializers.ModelSerializer):
     direction_link = serializers.SerializerMethodField('get_direction_link')
+    classes = ClassSerializer(many=True, read_only=True)
+    # schedule = serializers.SerializerMethodField('order_class')
 
     def get_direction_link(self, studio):
         return "https://www.google.com/maps/dir/?api=1&"+urlencode({'destination':studio.address})
 
+    # def order_class(self, studio)
     class Meta:
         model = Studio
-        fields = ('name', 'address', 'location', 'postcode', 'phone_number', 'direction_link')
+        fields = ('name', 'address', 'location', 'postcode', 'phone_number', 'direction_link', 'classes')
+
