92a2f093bb6531d6d99e0ef7b7e4b9b2a656e36c
Yifan Zhao <ethany.zhao@mail.utoronto.ca>
2022-11-14 22:45:58 -0500

    inline admin panel

--- fitness_club/studios/admin.py
+++ fitness_club/studios/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from .models import Studio, Class, Tag, StudioImage
 
+
 # Register your models here.
-admin.site.register(Studio)
-admin.site.register(Class)
-admin.site.register(Tag)
-admin.site.register(StudioImage)
+class StudioImageInline(admin.TabularInline):
+    model = StudioImage
+
+
+class StudioAdmin(admin.ModelAdmin):
+    inlines = [StudioImageInline]
+
+
+admin.site.register(Studio, StudioAdmin)
+
+
+class TagInline(admin.TabularInline):
+    model = Tag
+
+
+class ClassAdmin(admin.ModelAdmin):
+    inlines = [TagInline]
+
+
+admin.site.register(Class, ClassAdmin)
