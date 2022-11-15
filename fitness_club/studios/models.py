59d842597acea8da45aea9cce214ce021a11a2b9
Yifan Zhao <ethany.zhao@mail.utoronto.ca>
2022-11-14 21:08:39 -0500

    Studio model

--- fitness_club/studios/models.py
+++ fitness_club/studios/models.py
@@ -3,8 +3,7 @@ from django.db import models
 # from location_field.models.plain import PlainLocationField
 
 
-# Create your models here.
-# A studio has a name, address, geographical location, postal code, phone number, and a set of images.
+
 class Studio(models.Model):
     name = models.CharField(max_length=50,
                             blank=False,
@@ -17,7 +16,35 @@ class Studio(models.Model):
 
     postcode = models.CharField(max_length=7, blank=False, null=False)
     phone_number = models.CharField(max_length=20, blank=False, null=False)
+    # images = models.ManyToManyField(to=StudioImage, blank=True)
+
+
+class StudioImage(models.Model):
+    image = models.ImageField(upload_to="studio_images",
+                              blank=False,
+                              null=False)
+    studio = models.ForeignKey(to=Studio, on_delete=models.CASCADE)
+
+
+
 
 
 class Class(models.Model):
     studio = models.ForeignKey(Studio, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    capacity = models.IntegerField(blank=False, null=False)
+
+    start_time = models.DateTimeField(blank=False, null=False)
+    end_time = models.DateTimeField(blank=False, null=False)
+
+    def __str__(self):
+        return self.studio + self.name
+
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=50, blank=False, null=False)
+    studio = models.ForeignKey(Studio, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.tag_name
