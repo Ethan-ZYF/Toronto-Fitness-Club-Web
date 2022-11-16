a3646a2d20750a86263c360f07e9675888d0c202
Yifan Zhao <ethany.zhao@mail.utoronto.ca>
2022-11-15 21:43:44 -0500

    non-editable studio

--- fitness_club/studios/models.py
+++ fitness_club/studios/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class Studio(models.Model):
     name = models.CharField(max_length=50,
                             blank=False,
@@ -31,23 +32,30 @@ class StudioImage(models.Model):
     image = models.ImageField(upload_to="studio_images",
                               blank=False,
                               null=False)
-    studio = models.ForeignKey(to=Studio, on_delete=models.CASCADE, related_name="images")
+    studio = models.ForeignKey(to=Studio,
+                               on_delete=models.CASCADE,
+                               related_name="images")
 
 
 class Amenity(models.Model):
-    studio = models.ForeignKey(to=Studio, on_delete=models.CASCADE, related_name='amenities')
+    studio = models.ForeignKey(to=Studio,
+                               on_delete=models.CASCADE,
+                               related_name='amenities')
     type = models.CharField(max_length=50, blank=False, null=False)
     quantity = models.PositiveIntegerField(blank=False, null=False)
 
+
 class Class(models.Model):
-    studio = models.ForeignKey(Studio, on_delete=models.CASCADE, related_name='classes')
+    studio = models.ForeignKey(Studio,
+                               on_delete=models.CASCADE,
+                               related_name='classes')
     name = models.CharField(max_length=50, blank=False, null=False)
     description = models.TextField(blank=True, null=True)
     capacity = models.IntegerField(blank=False, null=False)
     coach = models.CharField(max_length=50, blank=True, null=True)
 
     start_date = models.DateTimeField(blank=False, null=False)
-    end_date = models.DateTimeField(blank=False, null=False) 
+    end_date = models.DateTimeField(blank=False, null=False)
     duration = models.PositiveIntegerField(blank=False, null=False)
 
     curr_capacity = models.PositiveBigIntegerField(default=capacity)
@@ -71,10 +79,17 @@ class Class(models.Model):
     def __str__(self):
         return str(self.studio) + " " + self.name
 
+
 class Event(models.Model):
-    belonged_class = models.ForeignKey(Class, on_delete=models.CASCADE, related_name='events')
+    belonged_class = models.ForeignKey(Class,
+                                       on_delete=models.CASCADE,
+                                       related_name='events')
     start_time = models.DateTimeField(blank=False, null=False)
-    studio = models.ForeignKey(Studio, on_delete=models.CASCADE, related_name='events')
+    studio = models.ForeignKey(Studio,
+                               on_delete=models.CASCADE,
+                               related_name='events',
+                               editable=False)
+
 
 @receiver(post_save, sender=Class)
 def createEvents(sender, **kwargs):
@@ -82,16 +97,19 @@ def createEvents(sender, **kwargs):
     instance = kwargs['instance']
 
     if instance.dates_changed:
-        Event.objects.filter(belonged_class = instance.id).filter(start_time__gt=timezone.now()).delete()
-        # create new future events 
+        Event.objects.filter(belonged_class=instance.id).filter(
+            start_time__gt=timezone.now()).delete()
+        # create new future events
         curr = instance.start_date
         while curr < timezone.now():
-            curr += timedelta(days = 7)
+            curr += timedelta(days=7)
         while curr < instance.end_date:
-            e=Event(belonged_class=instance, start_time=curr, studio=instance.studio)
+            e = Event(belonged_class=instance,
+                      start_time=curr,
+                      studio=instance.studio)
             e.save()
             print(e)
-            curr += timedelta(days = 7)
+            curr += timedelta(days=7)
 
 
 class Tag(models.Model):
@@ -105,6 +123,6 @@ class Tag(models.Model):
 class Loc(models.Model):
     latitude = models.FloatField(blank=False, null=False, default=0.0)
     longitude = models.FloatField(blank=False, null=False, default=0.0)
-    
+
     def __str__(self):
         return str(self.lon) + "," + str(self.lat)
