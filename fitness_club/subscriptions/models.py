2b5a69e604d7146481d9cc5d7253d6bf0644428e
Yifan Zhao <ethany.zhao@mail.utoronto.ca>
2022-11-15 22:53:22 -0500

    plan and subscriptions

--- fitness_club/subscriptions/models.py
+++ fitness_club/subscriptions/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from accounts.models import FCUser
+from studios.models import Studio
 
 # Create your models here.
+PLAN_CHOICES = (
+    ('MONTHLY', 'Monthly'),
+    ('YEARLY', 'Yearly'),
+)
+
+class Plan(models.Model):
+    gym = models.ForeignKey(to=Studio, on_delete=models.CASCADE, related_name='subscriptions')
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False, default=0.0)
+    
+    plan = models.CharField(max_length=50, choices=PLAN_CHOICES, default='YEARLY')
+
+
+    def __str__(self):
+        return f"{self.gym.name} - {self.member.username}"
+
+class Subscription(models.Model):
+    member = models.ForeignKey(to=FCUser, on_delete=models.CASCADE, related_name='subscriptions')
+    plan = models.ForeignKey(to=Plan, on_delete=models.CASCADE, related_name='subscriptions')
