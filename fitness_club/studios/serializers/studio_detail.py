8795b382f6830419938f326fa85b50fd3411f423
Yifan Zhao <ethany.zhao@mail.utoronto.ca>
2022-11-15 21:29:14 -0500

    update database and image field

--- fitness_club/studios/serializers/studio_detail.py
+++ fitness_club/studios/serializers/studio_detail.py
@@ -5,33 +5,44 @@ from django.utils.http import urlencode
 
 from django.utils import timezone
 
+
 class FilteredEventsSerializer(serializers.ListSerializer):
+
     def to_representation(self, data):
         data = data.filter(start_time__gt=timezone.now())
         return super(FilteredEventsSerializer, self).to_representation(data)
 
+
 class EventSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Event
-        fields = ("start_time",)
+        fields = ("start_time", )
         list_serializer_class = FilteredEventsSerializer
+
 
 class ClassSerializer(serializers.ModelSerializer):
     events = EventSerializer(many=True, read_only=True)
+
     class Meta:
         model = Class
-        fields = ('name', 'description','coach', 'curr_capacity', 'capacity', 'duration', 'events')
+        fields = ('name', 'description', 'coach', 'curr_capacity', 'capacity',
+                  'duration', 'events')
 
 
 class AmenitySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Amenity
         fields = ('type', 'quantity')
-        
+
+
 class StudioImageSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = StudioImage
-        fields = ('image',)
+        fields = ('image', )
+
 
 class StudioSerializer(serializers.ModelSerializer):
     direction_link = serializers.SerializerMethodField('get_direction_link')
@@ -39,13 +50,14 @@ class StudioSerializer(serializers.ModelSerializer):
     # sc = serializers.PrimaryKeyRelatedField(many=True, queryset=Event.objects.all())
     events = EventSerializer(many=True, read_only=True)
     amenities = AmenitySerializer(many=True, read_only=True)
-
+    images = StudioImageSerializer(many=True, read_only=True)
 
     def get_direction_link(self, studio):
-        return "https://www.google.com/maps/dir/?api=1&"+urlencode({'destination':studio.address})
+        return "https://www.google.com/maps/dir/?api=1&" + urlencode(
+            {'destination': studio.address})
 
     class Meta:
         model = Studio
-        fields = ('name', 'address', 'location', 'postcode', 'phone_number', 'direction_link', 'classes', 'events', 'amenities', 'images')
+        fields = ('name', 'address', 'location', 'postcode', 'phone_number',
+                  'direction_link', 'classes', 'events', 'amenities', 'images')
         # return future events in order of start_time
-
