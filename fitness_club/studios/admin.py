a8d49b2207b674d00bfeabe214e41c65649a9584
Yifan Zhao <ethany.zhao@mail.utoronto.ca>
2022-11-15 21:38:45 -0500

    event inline

--- fitness_club/studios/admin.py
+++ fitness_club/studios/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from .models import Studio, Class, Tag, StudioImage, Amenity
+from .models import Studio, Class, Tag, StudioImage, Amenity, Event
 
 
 # Register your models here.
 class StudioImageInline(admin.TabularInline):
     model = StudioImage
 
+
 class StudioAmenityInline(admin.TabularInline):
     model = Amenity
+
 
 class StudioAdmin(admin.ModelAdmin):
     inlines = [StudioImageInline, StudioAmenityInline]
@@ -20,8 +22,12 @@ class TagInline(admin.TabularInline):
     model = Tag
 
 
+class EventInline(admin.TabularInline):
+    model = Event
+
+
 class ClassAdmin(admin.ModelAdmin):
-    inlines = [TagInline]
+    inlines = [TagInline, EventInline]
 
 
 admin.site.register(Class, ClassAdmin)
