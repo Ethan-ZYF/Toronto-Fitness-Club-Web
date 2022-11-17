1896f2ce4d4dae0855b84cd912d478825205d6d9
zhuyuezx <zhuyuezx@gmail.com>
2022-11-17 04:32:36 -0500

    add future billing pages

--- fitness_club/accounts/urls.py
+++ fitness_club/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import SignupView, LoginView, LogoutView, EditView, PaymentHistoryView, PayView
+from .views import SignupView, LoginView, LogoutView, EditView, PaymentHistoryView, PayView, FuturePayView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
@@ -8,8 +8,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('edit/', EditView.as_view(), name='edit'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('payments_history/', PaymentHistoryView.as_view(), name='payments'),
+    path('payments/', PaymentHistoryView.as_view(), name='payments'),
     path('pay/', PayView.as_view(), name='pay'),
+    path('bill/', FuturePayView.as_view(), name='bill'),
     path("api/token/", TokenObtainPairView.as_view(),
          name="token_obtain_pair"),
 ]
