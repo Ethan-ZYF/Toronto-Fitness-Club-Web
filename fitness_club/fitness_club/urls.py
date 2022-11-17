b9faae30ec26e2916302a01091ad3aabf7d89241
zhuyuezx <zhuyuezx@gmail.com>
2022-11-16 20:19:06 -0500

    add list view of plans

--- fitness_club/fitness_club/urls.py
+++ fitness_club/fitness_club/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("studios/", include("studios.urls")),
+    path("subscriptions/", include("subscriptions.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
