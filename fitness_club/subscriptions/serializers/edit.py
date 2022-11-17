ff6d92d54e09e40c5964fe2c1e4438e5c97306f7
Yifan Zhao <ethany.zhao@mail.utoronto.ca>
2022-11-17 17:54:10 -0500

    trivial testing

--- fitness_club/subscriptions/serializers/edit.py
+++ fitness_club/subscriptions/serializers/edit.py
@@ -5,11 +5,13 @@ from django.utils import timezone
 from dateutil.relativedelta import relativedelta
 from rest_framework.response import Response
 
+
 class EditSubSerializer(serializers.ModelSerializer):
     new_start_date = serializers.SerializerMethodField('get_new_start_date')
-    
+
     def get_new_start_date(self, obj):
-        curr_subscription = Subscription.objects.get(user=self.context['request'].user)
+        curr_subscription = Subscription.objects.get(
+            user=self.context['request'].user)
         start_date = curr_subscription.start_date
         prev_plan = curr_subscription.plan.plan
         while start_date <= timezone.now().date():
@@ -18,26 +20,28 @@ class EditSubSerializer(serializers.ModelSerializer):
             else:
                 start_date += relativedelta(years=1)
         return start_date
-    
+
     class Meta:
         model = Subscription
         fields = ('new_start_date', 'plan')
-        read_only_fields = ('new_start_date',)
-    
+        read_only_fields = ('new_start_date', )
+
     def validate(self, data):
         # check if the user has the field subscription
         if not hasattr(self.context['request'].user, 'subscription'):
             raise serializers.ValidationError('You have not subscribed yet.')
         return data
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['plan'] = serializers.PrimaryKeyRelatedField(queryset=Plan.objects.all())
-    
+        self.fields['plan'] = serializers.PrimaryKeyRelatedField(
+            queryset=Plan.objects.all())
+
     def update(self, instance, validated_data):
         user = self.context['request'].user
         curr_subscription = Subscription.objects.get(user=user)
         curr_subscription.plan = validated_data['plan']
-        curr_subscription.start_date = self.get_new_start_date(curr_subscription)
+        curr_subscription.start_date = self.get_new_start_date(
+            curr_subscription)
         curr_subscription.save()
         return curr_subscription
--- fitness_club/subscriptions/views.py
+++ fitness_club/subscriptions/views.py
@@ -8,22 +8,24 @@ from subscriptions.serializers.edit import EditSubSerializer
 from django.contrib.auth import login, logout
 from accounts.models import FCUser
 from subscriptions.models import Plan, Subscription
+
 # from rest_framework.renderers import AdminRenderer
+
 
 # Create your views here.
 class PlansView(ListAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, )
     queryset = Plan.objects.all()
     serializer_class = PlanSerializer
-    
+
 
 class SubscribeView(CreateAPIView):
     serializer_class = SubscribeSerializer
-    permission_classes = (IsAuthenticated,)
-    
+    permission_classes = (IsAuthenticated, )
+
     def get(self, request, *args, **kwargs):
         return Response({'detail': 'Please choose a plan to enroll.'})
-    
+
     def post(self, request, *args, **kwargs):
         # check if the user has the field subscription
         if hasattr(request.user, 'subscription'):
@@ -37,24 +39,25 @@ class SubscribeView(CreateAPIView):
         print(request.user.active_subscription)
         return Response({'detail': 'You have successfully enrolled.'})
 
+
 class EditView(RetrieveAPIView, UpdateAPIView):
     serializer_class = EditSubSerializer
     permission_classes = (IsAuthenticated, )
-    
+
     def get_object(self):
         try:
             return Subscription.objects.get(user=self.request.user)
         except Subscription.DoesNotExist:
             return None
-    
+
+
 class CancelView(DestroyAPIView):
     permission_classes = (IsAuthenticated, )
     serializer_class = SubscribeSerializer
-    
+
     def get_queryset(self):
         return Subscription.objects.filter(user=self.request.user)
-        
-    
+
     def delete(self, request, *args, **kwargs):
         if hasattr(request.user, 'subscription'):
             # delete from queryset
@@ -62,13 +65,21 @@ class CancelView(DestroyAPIView):
             request.user.active_subscription = False
             # delete all the user's events in schedule that are later than start date
             request.user.save()
-            request.user.schedule.filter(start_time__gt=request.user.subscription.start_date).delete()
+            request.user.schedule.filter(
+                start_time__gt=request.user.subscription.start_date).delete()
+            print(request.user.schedule.all())
             request.user.save()
-            return Response({'detail': f'You have unsubscribed, expire_date: {request.user.subscription.start_date}.'})
+            return Response({
+                'detail':
+                f'You have unsubscribed, expire_date: {request.user.subscription.start_date}.'
+            })
         return Response({'detail': 'You are not subscribed.'})
-    
+
     def get(self, request, *args, **kwargs):
         # get current subscription
         if hasattr(request.user, 'subscription'):
-            return Response({'detail': f'You are currently subscribed to {str(request.user.subscription)}.'})
+            return Response({
+                'detail':
+                f'You are currently subscribed to {str(request.user.subscription)}.'
+            })
         return Response({'detail': 'You are not subscribed.'})
