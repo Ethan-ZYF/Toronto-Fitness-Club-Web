37cc290c27b214900169dd591037268839d2e0af
zhuyuezx <zhuyuezx@gmail.com>
2022-11-16 21:40:59 -0500

    add url to plan page

--- fitness_club/subscriptions/serializers/plan.py
+++ fitness_club/subscriptions/serializers/plan.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from subscriptions.models import Plan
 
 class PlanSerializer(serializers.ModelSerializer):
+    # url to subscribe page
+    url = serializers.HyperlinkedIdentityField(view_name='subscribe')
+    
     class Meta:
         model = Plan
-        fields = ['price', 'plan']
+        fields = ['price', 'plan', 'url']
--- fitness_club/subscriptions/urls.py
+++ fitness_club/subscriptions/urls.py
@@ -4,4 +4,5 @@ from subscriptions.views import PlansView, SubscribeView
 urlpatterns = [
     path('plans/', PlansView.as_view(), name='plans'),
     path('subscribe/', SubscribeView.as_view(), name='subscribe'),
+    path('subscribe/<int:pk>/', SubscribeView.as_view(), name='subscribe'),
 ]
