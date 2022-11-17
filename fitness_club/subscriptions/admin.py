72b1c19c4c0a9aab7d97633eac0b59735ff11b44
zhuyuezx <zhuyuezx@gmail.com>
2022-11-16 19:44:58 -0500

    add subs rule

--- fitness_club/subscriptions/admin.py
+++ fitness_club/subscriptions/admin.py
@@ -1,17 +1,7 @@
 from django.contrib import admin
 from .models import Subscription, Plan
+from rest_framework.exceptions import ValidationError
 
 
-# Register your models here.
-class PlanAdmin(admin.ModelAdmin):
-    def has_add_permission(self, request):
-        monthly = Plan.objects.filter(plan='MONTHLY').count()
-        yearly = Plan.objects.filter(plan='YEARLY').count()
-        if monthly >= 1 and yearly >= 1:
-            return False
-        else:
-            return True
-
-
-admin.site.register(Plan, PlanAdmin)
+admin.site.register(Plan)
 admin.site.register(Subscription)
--- fitness_club/subscriptions/models.py
+++ fitness_club/subscriptions/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from accounts.models import FCUser
 from studios.models import Studio
 from django.utils import timezone
+from rest_framework.exceptions import ValidationError
 
 # Create your models here.
 PLAN_CHOICES = (
@@ -9,6 +10,17 @@ PLAN_CHOICES = (
     ('YEARLY', 'Yearly'),
 )
 
+PLAN_CHOICE_MONTHLY = (('MONTHLY', 'Monthly'),)
+
+PLAN_CHOICE_YEARLY = (('YEARLY', 'Yearly'),)
+
+def validate_only_one_month_and_one_year(obj):
+    month_cnt = Plan.objects.filter(plan='MONTHLY').count()
+    year_cnt = Plan.objects.filter(plan='YEARLY').count()
+    if obj.plan == 'MONTHLY' and month_cnt >= 1:
+        raise ValidationError("Monthly plan already exists")
+    if obj.plan == 'YEARLY' and year_cnt >= 1:
+        raise ValidationError("Yearly plan already exists")
 
 class Plan(models.Model):
     price = models.DecimalField(max_digits=6,
@@ -20,7 +32,13 @@ class Plan(models.Model):
                             choices=PLAN_CHOICES,
                             blank=True,
                             null=True)
-
+    
+    def clean(self):
+        try:
+            validate_only_one_month_and_one_year(self)
+        except ValidationError:
+            pass
+    
     def __str__(self):
         return f"{self.plan} - {self.price}"
 
