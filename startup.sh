c26efbc60836d274af5f7b3edc5c1343fc605076
Yifan Zhao <ethany.zhao@mail.utoronto.ca>
2022-11-18 20:43:41 -0500

    final version

--- startup.sh
+++ startup.sh
@@ -2,3 +2,8 @@ python3 -m virtualenv ./venv
 source ./venv/bin/activate
 
 pip3 install -r requirements.txt
+
+cd fitness_club
+python3 manage.py makemigrations accounts
+python3 manage.py makemigrations studios
+python3 manage.py migrate
