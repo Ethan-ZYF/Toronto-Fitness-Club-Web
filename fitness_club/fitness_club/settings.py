01aa60d103814c5db8096e52c8c8caff0177a8cc
Yifan Zhao <ethany.zhao@mail.utoronto.ca>
2022-12-05 21:33:59 -0500

    some minor issues

--- fitness_club/fitness_club/settings.py
+++ fitness_club/fitness_club/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-mnkb1#6s2&w0efss7mvuusn*j6i@e@xw$ild#!p6j^dh-!_ai4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -86,6 +87,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
--- fitness_club_frontend/src/Pages/SignInPage.js
+++ fitness_club_frontend/src/Pages/SignInPage.js
@@ -14,172 +14,174 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { userContext, loggedInState } from '../userContext';
 
-import {useState, useEffect, useContext}from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { login } from '../api';
 import { validateSignInForm } from './utils/validators';
 import { Navigate } from "react-router-dom";
 
 
 const Copyright = (props) => {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Toronto Fitness Club
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Toronto Fitness Club
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
 const theme = createTheme();
 
 const SignIn = () => {
 
-  const [username, setUserName] = useState('');
-  const [password, setPassword] = useState('');
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
 
 
-  const [signInSuccess, setSignInSuccess] = useState(false);
-  const [isFormValid, setIsFormValid] = useState(false);
-  const [errorMsg, setErrorMsg] = useState(false);
+    const [signInSuccess, setSignInSuccess] = useState(false);
+    const [isFormValid, setIsFormValid] = useState(false);
+    const [errorMsg, setErrorMsg] = useState(false);
 
-  const context = useContext(userContext);
-  
-  useEffect(
-    () => {
-      setIsFormValid(validateSignInForm({username, password}));
-    },
-    [username, password]
-  );
+    const context = useContext(userContext);
 
-  const handleSignUp =  async(event) => {
-    event.preventDefault();
-
-    const userLoginInfo = {
-      username,
-      password,
-    }
-    console.log(userLoginInfo)
-
-    login(userLoginInfo)
-    .then((response) => {
-      console.log(response);
-      setErrorMsg("");
-      const user = {...userLoginInfo, token:response.data.access}
-      console.log('user', user)
-      context.setContext(loggedInState(user.username, user.token));
-      console.log(context)
-      localStorage.setItem('user', JSON.stringify(user));
-      setSignInSuccess(true);
-    })
-    .catch((error) => {
-      setErrorMsg(error.response);
-    })
-  };
-  
-  if (signInSuccess) {
-    return <Navigate replace to="/dashboard" />;
-  }
-
-  if (localStorage.getItem('user') != null) {
-    return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-                marginTop: 8,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-            }}>
-            <Typography component="h1" variant="h5">
-                You have already signed in, continue to dashboard <NavLink to='/dashboard'>here</NavLink>!
-            </Typography>
-        </Box>
-      </Container>
-    </ThemeProvider>
+    useEffect(
+        () => {
+            setIsFormValid(validateSignInForm({ username, password }));
+        },
+        [username, password]
     );
-  }
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              User Sign In
-            </Typography>
+    const handleSignUp = async (event) => {
+        event.preventDefault();
 
-            <Box component="form" onSubmit={handleSignUp} sx={{ mt: 2}}>
-              <Grid container spacing={2}>
-              {errorMsg.detail && <p style={{fontSize:'0.8rem', color:'#D7272D', paddingLeft:'1rem'}}>Please try again: {errorMsg.detail}</p>}
-               
-              <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="username"
-                    label="Username"
-                    name="username"
-                    value={username}
-                    onChange={(e) => setUserName(e.target.value)}
-                    error={errorMsg.username}
-                    helperText={errorMsg.username}
-                  />
-                </Grid>
+        const userLoginInfo = {
+            username,
+            password,
+        }
+        console.log(userLoginInfo)
 
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </Grid>
-              </Grid>
+        login(userLoginInfo)
+            .then((response) => {
+                // console.log(response);
+                setErrorMsg("");
+                console.log("response.data", response.data);
+                const user = { ...userLoginInfo, token: response.data.access }
+                console.log('user', user)
+                context.setContext(loggedInState(user.username, user.token));
+                console.log(context)
+                localStorage.setItem('user', JSON.stringify(user));
+                setSignInSuccess(true);
+            })
+            .catch((error) => {
+                console.log("err", error);
+                setErrorMsg(error.response);
+            })
+    };
 
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                <NavLink variant="body2" to='/signup'>
-                {"Don't have an account? Sign Up"}
-                </NavLink>
+    if (signInSuccess) {
+        return <Navigate replace to="/dashboard" />;
+    }
 
-                </Grid>
-              </Grid>
+    if (localStorage.getItem('user') != null) {
+        return (
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}>
+                        <Typography component="h1" variant="h5">
+                            You have already signed in, continue to dashboard <NavLink to='/dashboard'>here</NavLink>!
+                        </Typography>
+                    </Box>
+                </Container>
+            </ThemeProvider>
+        );
+    }
 
-            </Box>
-          </Box>
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        User Sign In
+                    </Typography>
 
-        <Copyright sx={{ mt: 8}} />
-      </Container>
-    </ThemeProvider>
-  );
+                    <Box component="form" onSubmit={handleSignUp} sx={{ mt: 2 }}>
+                        <Grid container spacing={2}>
+                            {errorMsg.detail && <p style={{ fontSize: '0.8rem', color: '#D7272D', paddingLeft: '1rem' }}>Please try again: {errorMsg.detail}</p>}
+
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="username"
+                                    label="Username"
+                                    name="username"
+                                    value={username}
+                                    onChange={(e) => setUserName(e.target.value)}
+                                    error={errorMsg.username}
+                                    helperText={errorMsg.username}
+                                />
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <NavLink variant="body2" to='/signup'>
+                                    {"Don't have an account? Sign Up"}
+                                </NavLink>
+
+                            </Grid>
+                        </Grid>
+
+                    </Box>
+                </Box>
+
+                <Copyright sx={{ mt: 8 }} />
+            </Container>
+        </ThemeProvider>
+    );
 }
 
 export default SignIn;
