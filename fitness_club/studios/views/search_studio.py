816c97cc6ffa04f214a9ef07f0fbf0cd40f03199
zhuyuezx <zhuyuezx@gmail.com>
2022-12-07 01:13:46 -0500

    complete location search

--- fitness_club/studios/views/search_studio.py
+++ fitness_club/studios/views/search_studio.py
@@ -6,31 +6,31 @@ from studios.serializers.search import SearchSerializer
 from studios.serializers.all_studios import ListSerializer, FilterLocationSerializer
 import copy
 
-class SearchView(ListAPIView):
+class SearchView(ListCreateAPIView):
     serializer_class = FilterLocationSerializer
     permission_classes = (IsAuthenticated,)
     model = Studio
-    
-    def __init__(self, *args, **kwargs):
-        super(SearchView, self).__init__(*args, **kwargs)
-        self.target_lat = 0.0
-        self.target_lon = 0.0
+
+    def __init__(self):
+        super().__init__()
+        with open('studios/views/target_loc.txt', 'r') as f:
+            self.target_lat, self.target_lon = f.read().split(',')
+        print(f'init:{self.target_lat}, {self.target_lon}')
 
     # post request to search for studios based on location
     def post(self, request, *args, **kwargs):
-        # print(f'post:{self.target_lat}, {self.target_lon}')
-        self.target_lat = request.POST.get('latitude')
-        self.target_lon = request.POST.get('longitude')
+        with open('studios/views/target_loc.txt', 'w') as f:
+            f.write(request.data['location'])
+            print(f'write:{request.data["location"]}')
         return Response({'Success': 'Location received'})
 
     # def get(self, request, *args, **kwargs):
     #     return Response({'success': 'Class created'})
     
     def get_queryset(self):
+        all_studios = Studio.objects.all()
         lat = float(self.target_lat)
         lon = float(self.target_lon)
-        all_studios = Studio.objects.all()
-        print(len(all_studios))
         studio_list = sorted(all_studios, key=lambda x: (float(x.location.split(',')[0]) - lat) ** 2 + (float(x.location.split(',')[1]) - lon) ** 2)
         
         return studio_list
