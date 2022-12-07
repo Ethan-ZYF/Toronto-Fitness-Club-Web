acc7a999c10fd4d212e5758adef114abb02dcb2b
Yifan Zhao <ethany.zhao@mail.utoronto.ca>
2022-12-06 22:37:25 -0500

    studios images added

--- fitness_club_frontend/src/Pages/StudioDetail.js
+++ fitness_club_frontend/src/Pages/StudioDetail.js
@@ -13,6 +13,11 @@ import TextField from '@mui/material/TextField';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getStudioDetail } from '../api';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+// import MapIcon from '@mui/icons-material/Map';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const theme = createTheme();
 
@@ -29,29 +34,65 @@ const Item = styled(Paper)(({ theme }) => ({
     marginBottom: '20px',
 }));
 
+let itemData = []
+
+
+const StandardImageList = () => {
+    console.log("itemData", itemData);
+    return (
+        <ImageList
+            sx={{
+                width: 500,
+                height: 328,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                marginTop: '20px',
+            }}
+            cols={3}
+            rowHeight={164}
+        >
+            {itemData.map((item) => (
+                <ImageListItem key={item}>
+                    <img
+                        src={`${item.image}?w=164&h=164&fit=crop&auto=format`}
+                        srcSet={`${item.image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                        alt="https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png"
+                        loading="lazy"
+                    />
+                </ImageListItem>
+            ))}
+        </ImageList>
+    );
+}
+
+
+
 export default function StudioDetail() {
     const [detail, setDetail] = useState({});
     const { id } = useParams();
     useEffect(() => {
         getStudioDetail(id)
-        .then((response) => {
-            setDetail(response.data);
-            console.log(response.data)
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    }, []);
-
+            .then((response) => {
+                setDetail(response.data);
+                // console.log(response.data)
+                itemData = response.data.images;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    });
+    console.log("details", detail);
+    console.log("images", detail.images);
     return (
         <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
+            <Container component="main" sx={{
+                width: 700
+            }}>
+                <StandardImageList />
+
                 <CssBaseline />
                 <Box sx={{ flexGrow: 1 }}>
-                    <Item>
-                        
-                    </Item>
-                    <Grid container rowSpacing={0} columnSpacing={0} paddingTop={10}>
+                    <Grid container rowSpacing={0} columnSpacing={0} paddingTop={0}>
                         <Grid item xs={6}>
                             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} fontWeight='bold'>
                                 Studio Name:
@@ -69,17 +110,23 @@ export default function StudioDetail() {
                         </Grid>
                         <Grid item xs={6}>
                             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align='right'>
-                                {detail['address']? detail['address'] : 'Not Provided'}
+                                {detail['address'] ? detail['address'] : 'Not Provided'}
                             </Typography>
                         </Grid>
                         <Grid item xs={6}>
                             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} fontWeight='bold'>
-                                Direction:
+                                Map:
                             </Typography>
                         </Grid>
                         <Grid item xs={6}>
                             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align='right'>
-                                {detail['direction_link']? <a href={detail['direction_link']} target='_blank'>Google Map Link</a> : 'Not Provided'}
+                                {
+                                    detail['direction_link'] ?
+                                        <a href={detail['direction_link']} target='_blank'>
+                                            <LocationOnIcon />
+                                        </a> :
+                                        'Not Provided'
+                                }
                             </Typography>
                         </Grid>
                         <Grid item xs={6}>
@@ -89,7 +136,7 @@ export default function StudioDetail() {
                         </Grid>
                         <Grid item xs={6}>
                             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align='right'>
-                                {detail['postcode']? detail['postcode'] : 'Not Provided'}
+                                {detail['postcode'] ? detail['postcode'] : 'Not Provided'}
                             </Typography>
                         </Grid>
                         <Grid item xs={6}>
@@ -99,7 +146,7 @@ export default function StudioDetail() {
                         </Grid>
                         <Grid item xs={6}>
                             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align='right'>
-                                {detail['phone_number']? detail['phone_number'] : 'Not Provided'}
+                                {detail['phone_number'] ? detail['phone_number'] : 'Not Provided'}
                             </Typography>
                         </Grid>
                     </Grid>
